1490c8bf7050a5113934114522f2ab4910d9f4b5
Ahmad Taufiq <ahmadtaufiq879@gmail.com>
2025-04-21 23:34:37 +0700

    [ADD] Missing redis service

---
+++ service/redis_service.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"time"
+
+	"qiscus-omnichannel/repository"
+)
+
+type RedisService interface {
+	SetCache(key string, value string, ttl time.Duration) error
+	GetCache(key string) (string, error)
+	SetJSON(key string, value interface{}, ttl time.Duration) error
+	GetJSON(key string, target interface{}) error
+	UpdateJSONField(key string, jsonPath string, value interface{}) error
+	DeleteJSON(key string) error
+}
+
+type redisService struct {
+	repo repository.RedisRepository
+}
+
+func NewRedisService(repo repository.RedisRepository) RedisService {
+	return &redisService{repo: repo}
+}
+
+func (s *redisService) SetCache(key string, value string, ttl time.Duration) error {
+	return s.repo.SetCache(key, value, ttl)
+}
+
+func (s *redisService) GetCache(key string) (string, error) {
+	return s.repo.GetCache(key)
+}
+
+func (s *redisService) SetJSON(key string, value interface{}, ttl time.Duration) error {
+	return s.repo.SetJSON(key, value, ttl)
+}
+
+func (s *redisService) GetJSON(key string, target interface{}) error {
+	return s.repo.GetJSON(key, target)
+}
+
+func (s *redisService) UpdateJSONField(key string, jsonPath string, value interface{}) error {
+	return s.repo.UpdateJSONField(key, jsonPath, value)
+}
+
+func (s *redisService) DeleteJSON(key string) error {
+	return s.repo.DeleteJSON(key)
+}
